e251535dc9e8ba20a4c8df95b9e21b1536c993f1
lashwi <alamani2@illinois.edu>
2023-10-28 16:59:03 -0500

    Attend to review comments, add chakra provider at parent level

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import './globals.css';
 
 export const metadata = {
@@ -8,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body><ChakraProvider>{children}</ChakraProvider></body>
         </html>
     );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -3,20 +3,15 @@ import Image from 'next/image';
 import { ChakraProvider } from '@chakra-ui/react'
 
 // Added this for testing, can be removed later
-import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
+import { Card, CardBody } from '@chakra-ui/react'
 
 export default function Home() {
     return (
-        <ChakraProvider>
-            <div>
-                {/* Delete the SampleComponent before starting work on the page */}
-                <SampleComponent samplePropTitle="This is a Sample Component" />
-            </div>
             <Card>
                 <CardBody>
+                    {/* Delete the SampleComponent before starting work on the page */}
                     <SampleComponent samplePropTitle="This is a Sample Component" />
                 </CardBody>
             </Card>
-        </ChakraProvider>
     );
 }
