e0f235f4c1f2c5341559c40601b6dd667cab30b3
lashwi <alamani2@illinois.edu>
2023-10-28 18:02:19 -0500

    Test

--- .github/hooks/pre-receive.sh
+++ .github/hooks/pre-receive.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-BRANCH_PATTERN="^(feat|bug|test)/"
+BRANCH_PATTERN="(feat|bug|test)/"
 
 if ! [[ $1 =~ $BRANCH_PATTERN ]]; then
   echo "Branch name must match the following pattern: $BRANCH_PATTERN"
