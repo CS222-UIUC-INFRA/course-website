29be0a2c830c597b2c0d5ab31e01b4d07ae1c876
lashwi <143149974+lashwi@users.noreply.github.com>
2023-10-29 15:37:30 -0500

    added integration with chakra ui (#3)
    * added integration with chakra ui
    
    * remove package-lock.json
    
    * Attend to review comments, add chakra provider at parent level

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,17 @@
 import SampleComponent from '@/components/SampleComponent/SampleComponent';
 import Image from 'next/image';
+import { ChakraProvider } from '@chakra-ui/react'
+
+// Added this for testing, can be removed later
+import { Card, CardBody } from '@chakra-ui/react'
 
 export default function Home() {
     return (
-        <div>
-            {/* Delete the SampleComponent before starting work on the page */}
-            <SampleComponent samplePropTitle="This is a Sample Component" />
-        </div>
+            <Card>
+                <CardBody>
+                    {/* Delete the SampleComponent before starting work on the page */}
+                    <SampleComponent samplePropTitle="This is a Sample Component" />
+                </CardBody>
+            </Card>
     );
 }
