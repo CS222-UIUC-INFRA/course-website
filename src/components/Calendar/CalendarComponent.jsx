299993d08106ffd094d29494fcab5ba5a1eda102
You Lyu <youlyu2@outlook.com>
2023-11-09 14:08:14 -0800

    finished calendar

--- src/components/Calendar/CalendarComponent.jsx
+++ src/components/Calendar/CalendarComponent.jsx
@@ -5,8 +5,6 @@ import { Card, CardHeader, CardBody, CardFooter, Image, Stack, Heading, Text, Bu
 
 const formatPictureUrl = (url) => {
     let entry_id = url.split('/')[5];
-    console.log(entry_id);
-    entry_id = "1_m2oon9t3"
     let res = `https://cfvod.kaltura.com/p/1329972/sp/132997200/thumbnail/entry_id/${entry_id}/width/478/height/269/type/3/quality/100/vid_slice/1/vid_slices/16`;
 
     return res;
@@ -17,47 +15,57 @@ function CalendarCard({ data }) {
 
     return (
         <Card
-            direction={{ base: 'column', sm: 'row' }}
+            className="h-64"
+            direction='row'
             overflow='hidden'
             variant='outline'
         >
-            <a href={data.Recording}>
-                <Image
+            <a href={data.Recording} className="h-full w-1/3 border">
+                <img
+                    className="h-full w-full object-cover"
                     src={formatPictureUrl(data.Recording)}
-                    alt='Media Place Preview'
+                    alt='Lecture Image'
                 />
             </a>
 
-            <Stack>
-                <CardBody>
-                    <Heading pt='2' size='xs' textTransform='uppercase'>
+
+
+            <CardBody className="h-full w-2/3 flex flex-col justify-between">
+
+                <div className="flex flex-col gap-2">
+                    <Heading pt='2' size='sm'>
                         {data.Date}
                     </Heading>
 
                     <Heading size='lg'>{data.LectureContent}</Heading>
+                </div>
 
-                </CardBody>
-
-                <CardFooter>
+                <div
+                    className="flex flex-col gap-2"
+                >
                     <a href={data.Recording}>
-                        <Button className="m-2" variant='solid' colorScheme='blue'>
-                            Watch Recording Now
+                        <Button variant='solid' colorScheme='blue' >
+                            Watch Recording
                         </Button>
                     </a>
-                    
+
 
                     {Object.entries(data.Appendix).map(([label, link], index) => (
                         <a href={link}>
-                            <Button className="m-2" variant='outline' colorScheme='blue' key={index}>
-                                See {label}
+                            <Button variant='outline' colorScheme='blue' key={index}>
+                                {label}
                             </Button>
                         </a>
 
                     )
                     )}
 
-                </CardFooter>
-            </Stack>
+                </div>
+
+            </CardBody>
+
+
+
         </Card>
     )
 }
@@ -65,13 +73,15 @@ function CalendarCard({ data }) {
 export default function CalendarComponent({ calendarJson }) {
     return (
         // This is how you use module scoped CSS (use this method only if TailwindCSS isn't able to handle the usecase)
-        <div className={styles.parent}>
-            {/* This is how you use TailwindCSS */}
-            {
-                calendarJson.Content.map((data, index) => (
-                    <CalendarCard key={index} data={data} />
-                ))
-            }
+        <div className={styles.container}>
+            <div className="flex flex-col gap-4 p-20">
+                {/* This is how you use TailwindCSS */}
+                {
+                    calendarJson.Content.map((data, index) => (
+                        <CalendarCard className="m-20" key={index} data={data} />
+                    ))
+                }
+            </div>
         </div>
     );
 }
