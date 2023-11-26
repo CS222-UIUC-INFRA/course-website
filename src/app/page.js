9fae0b42e166d5e6dfdec00fb6c787d0f5e91f48
lashwi <alamani2@illinois.edu>
2023-11-26 16:09:24 -0600

    add tabs to access different sections of course info

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,3 @@
-<<<<<<< Updated upstream
-import SampleComponent from '@/components/SampleComponent/SampleComponent';
-import Image from 'next/image';
-import { ChakraProvider } from '@chakra-ui/react'
-
-// Added this for testing, can be removed later
-import { Card, CardBody } from '@chakra-ui/react'
-
-export default function Home() {
-    return (
-            <Card>
-                <CardBody>
-                    {/* Delete the SampleComponent before starting work on the page */}
-                    <SampleComponent samplePropTitle="This is a Sample Component" />
-                </CardBody>
-            </Card>
-=======
 export default function Home() {
     return (
         <div>
@@ -22,6 +5,6 @@ export default function Home() {
             Welcome to CS222!
             </span>
         </div>
->>>>>>> Stashed changes
+
     );
 }
