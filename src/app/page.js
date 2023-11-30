ce8d4f57592210cc382a31b52f2609bcc5a0599b
Vedang <76455633+vedangBh@users.noreply.github.com>
2023-11-30 15:54:55 -0600

    Main merge

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
-// import SampleComponent from '@/components/SampleComponent/SampleComponent';
-// import Image from 'next/image';
 
-// export default function Home() {
-//     return (
-//         <div>
-//             {/* Delete the SampleComponent before starting work on the page */}
-//             <SampleComponent samplePropTitle="This is a Sample Component" />
-//         </div>
-//     );
-// }
+export default function Home() {
+  return (
+      <div>
+          {/* Content of the Home component */}
+      </div>
+  );
+}
+
